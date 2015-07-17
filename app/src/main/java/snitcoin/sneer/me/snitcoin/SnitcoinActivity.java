29c8348d2d4a6bf3523d910578dbdf68d2e26f75
Eric Rodrigo de freitas <eric_freitas@hotmail.com>
2015-07-17 19:59:11 -0300

    refatoracao

--- app/src/main/java/snitcoin/sneer/me/snitcoin/SnitcoinActivity.java
+++ app/src/main/java/snitcoin/sneer/me/snitcoin/SnitcoinActivity.java
@@ -3,22 +3,30 @@ package snitcoin.sneer.me.snitcoin;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import org.bitcoinj.core.AbstractWalletEventListener;
 import org.bitcoinj.core.AddressFormatException;
-import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.InsufficientMoneyException;
+import org.bitcoinj.core.Monetary;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.Wallet;
 import org.bitcoinj.params.TestNet3Params;
+import org.bitcoinj.utils.MonetaryFormat;
 
 
 public class SnitcoinActivity extends ActionBarActivity {
 
     SnitcoinAppKit kit;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +34,88 @@ public class SnitcoinActivity extends ActionBarActivity {
         setContentView(R.layout.activity_snitcoin);
 
         kit = new SnitcoinAppKit(this, getApplicationContext(), TestNet3Params.get());
-        ((Button) findViewById(R.id.button_send)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String address = ((EditText) findViewById(R.id.edit_text_address)).getText().toString();
-                String amount = ((EditText) findViewById(R.id.edit_text_amount)).getText().toString();
-
-                try {
-                    kit.send(amount,address);
-                } catch (AddressFormatException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        kit.wallet().addEventListener(new WalletEventListener().invoke());
+        Log.d("Current Receive Address", kit.wallet().currentReceiveAddress().toString());
+        ((TextView) findViewById(R.id.address)).setText(kit.wallet().currentReceiveAddress().toString());
+        ((TextView) findViewById(R.id.balance)).setText(format(kit.wallet().getBalance()));
+        ((Button) findViewById(R.id.button_send)).setOnClickListener(new SendActionListener().invoke());
     }
+
+    public String format(Monetary monetary){
+        return MonetaryFormat.BTC.format(monetary).toString();
+    }
+
+    public class WalletEventListener {
+        public AbstractWalletEventListener invoke() {
+            return new AbstractWalletEventListener() {
+                @Override
+                public void onCoinsReceived(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+                    ((TextView) findViewById(R.id.balance)).setText(format(wallet.getBalance()));
+                    String title = "Coins Received";
+                    String text = getNotifyText(wallet, tx, prevBalance, newBalance);
+                    Log.d(title, text);
+                }
+
+                @Override
+                public void onCoinsSent(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+                    ((TextView) findViewById(R.id.balance)).setText(format(wallet.getBalance()));
+                    String title = "Coins Sent";
+                    String text = getNotifyText(wallet, tx, prevBalance, newBalance);
+                    Log.d(title, text);
+                }
+
+                @Override
+                public void onWalletChanged(Wallet wallet) {
+                    ((TextView) findViewById(R.id.balance)).setText(format(wallet.getBalance()));
+                    String title = "Wallet chaged!";
+                    String text = "Current Balance:" + format(wallet.getBalance());
+                    Log.d(title, text);
+                }
+            };
+        }
+
+        private String  getNotifyText(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Transaction: " + tx.getHashAsString()).append("\n");
+            sb.append("Received Amount: " + format(tx.getValue(wallet))).append("\n");
+            sb.append("Prev Balance: " + format(prevBalance)).append("\n");
+            sb.append("New Balance: " + format(newBalance)).append("\n");
+            return sb.toString();
+        }
+    }
+
+    private class SendActionListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String address = ((EditText) findViewById(R.id.edit_text_address)).getText().toString();
+            String amount = ((EditText) findViewById(R.id.edit_text_amount)).getText().toString();
+            Coin value = Coin.parseCoin(amount);
+
+            try {
+                Wallet.SendResult result = kit.send(value, address);
+
+            } catch (AddressFormatException e) {
+                e.printStackTrace();
+
+            } catch (InsufficientMoneyException e) {
+                Toast.makeText(getApplicationContext(), "Not enough coins in your wallet. Missing " + e.missing.getValue() + " satoshis are missing (including fees)", Toast.LENGTH_LONG).show();
+                ListenableFuture<Coin> balanceFuture = kit.wallet().getBalanceFuture(value, Wallet.BalanceType.AVAILABLE);
+                FutureCallback<Coin> callback = new InsufficientMoneyFutureCallback();
+                Futures.addCallback(balanceFuture, callback);
+            } catch (Wallet.DustySendRequested e) {
+                Toast.makeText(getApplicationContext(), "Too small amount", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private class InsufficientMoneyFutureCallback implements FutureCallback<Coin> {
+        public void onSuccess(Coin result) {
+            Toast.makeText(getApplicationContext(), "coins arrived and the wallet now has enough balance", Toast.LENGTH_LONG).show();
+        }
+
+        public void onFailure(Throwable t) {
+            Toast.makeText(getApplicationContext(), "something went wrong", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
--- app/src/main/java/snitcoin/sneer/me/snitcoin/SnitcoinAppKit.java
+++ app/src/main/java/snitcoin/sneer/me/snitcoin/SnitcoinAppKit.java
@@ -2,100 +2,39 @@ package snitcoin.sneer.me.snitcoin;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.AsyncTask;
-import android.util.Log;
-import android.widget.TextView;
 
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListenableFuture;
-
-import org.bitcoinj.core.AbstractWalletEventListener;
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.AddressFormatException;
 import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.InsufficientMoneyException;
 import org.bitcoinj.core.NetworkParameters;
-import org.bitcoinj.core.Transaction;
 import org.bitcoinj.core.Wallet;
-import org.bitcoinj.core.Wallet.BalanceType;
 import org.bitcoinj.kits.WalletAppKit;
 
 
-
 public class SnitcoinAppKit{
-
     private WalletAppKit kit;
     private Activity activity;
     private Context context;
     private NetworkParameters params;
 
-    public SnitcoinAppKit(Activity activity, Context context, NetworkParameters params) {
+    public SnitcoinAppKit(final Activity activity, Context context, NetworkParameters params) {
         this.activity = activity;
         this.context = context;
         this.params = params;
 
-        Log.d("[DEBUG]", "Opening snitcoin-wallet file");
         kit = new WalletAppKit(params, context.getFilesDir(), "snitcoin-wallet");
         kit.setAutoSave(true);
         kit.startAsync();
-
-        Log.d("[DEBUG]", "AwaitRunning");
         kit.awaitRunning();
-
-        Log.d("[DEBUG]", "Balance: " + kit.wallet().getBalance().getValue());
-        Log.d("[DEBUG]", "Send money to: " + kit.wallet().currentReceiveAddress().toString());
-
-        ((TextView) activity.findViewById(R.id.address)).setText(kit.wallet().currentReceiveAddress().toString());
-        ((TextView) activity.findViewById(R.id.balance)).setText(kit.wallet().getBalance().toString());
-
-        kit.wallet().addEventListener(new AbstractWalletEventListener() {
-            @Override
-            public void onCoinsReceived(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
-                Log.d("[DEBUG]", "-----> coins resceived: " + tx.getHashAsString());
-                Log.d("[DEBUG]", "received: " + tx.getValue(wallet));
-                Log.d("[DEBUG]", "prev balance: " + prevBalance.getValue());
-                Log.d("[DEBUG]", "new balance: " + newBalance.getValue());
-            }
-
-            @Override
-            public void onCoinsSent(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance) {
-                Log.d("[DEBUG]", "-----> coins sent: " + tx.getHashAsString());
-                Log.d("[DEBUG]", "coins sent: " + tx.getValue(wallet));
-                Log.d("[DEBUG]", "prev balance: " + prevBalance.getValue());
-                Log.d("[DEBUG]", "new balance: " + newBalance.getValue());
-            }
-
-            @Override
-            public void onWalletChanged(Wallet wallet) {
-                Log.d("[DEBUG]", "Wallet chaged! Current Balance: " + wallet.getBalance().getValue());
-            }
-        });
     }
 
-    public void send(String amount, String address) throws AddressFormatException{
-        Address to = new Address(params, address);
-        Coin value = Coin.parseCoin(amount);
-        try {
-            Wallet.SendResult result = kit.wallet().sendCoins(kit.peerGroup(), to, value);
+    public Wallet wallet(){
+        return kit.wallet();
+    }
 
-            Log.d("[DEBUG]","-----> " + kit.wallet().currentReceiveAddress());
-            Log.d("[DEBUG]","coins sent. transaction hash: " + result.tx.getHashAsString());
-        } catch (InsufficientMoneyException e) {
-            System.err.println("-----> " + kit.wallet().currentReceiveAddress());
-            System.err.println("Not enough coins in your wallet. Missing " + e.missing.getValue() + " satoshis are missing (including fees)");
-
-            ListenableFuture<Coin> balanceFuture = kit.wallet().getBalanceFuture(value, BalanceType.AVAILABLE);
-            FutureCallback<Coin> callback = new FutureCallback<Coin>() {
-                public void onSuccess(Coin balance) {
-                    Log.d("[DEBUG]","coins arrived and the wallet now has enough balance");
-                }
-
-                public void onFailure(Throwable t) {
-                    Log.d("[DEBUG]","something went wrong");
-                }
-            };
-            Futures.addCallback(balanceFuture, callback);
-        }
+    public Wallet.SendResult send(Coin coin, String address) throws AddressFormatException, InsufficientMoneyException, Wallet.DustySendRequested{
+        Wallet.SendResult result = kit.wallet().sendCoins(kit.peerGroup(), (new Address(params, address)), coin);
+        return result;
     }
 }
