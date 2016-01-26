9ef556b5e2885fe80c270c74e8d695bea0def4d9
Eric Rodrigo de freitas <eric_freitas@hotmail.com>
2016-01-25 22:55:01 -0200

    teste

--- app/src/main/java/snitcoin/sneer/me/snitcoin/ExchangeActivity.java
+++ app/src/main/java/snitcoin/sneer/me/snitcoin/ExchangeActivity.java
@@ -72,7 +72,7 @@ public class ExchangeActivity extends Activity {
         builder.setPositiveButton("Request", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                
+
             }
         });
         setNegativeButton(builder);
